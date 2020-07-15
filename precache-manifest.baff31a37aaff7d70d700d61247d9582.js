afeb11822c25b8014b0b5e99dfbb3dadf355067a
Soe Thura <thixpin@gmail.com>
2020-07-15 21:55:36 +0630

    Updates

---
+++ precache-manifest.baff31a37aaff7d70d700d61247d9582.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4463f6259624f007394c5735ec45fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4032fff733df3d934094",
+    "url": "/static/css/main.7fbd88ef.chunk.css"
+  },
+  {
+    "revision": "fcf885c3fe2e80175fbd",
+    "url": "/static/js/2.61c685d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.61c685d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4032fff733df3d934094",
+    "url": "/static/js/main.28487819.chunk.js"
+  },
+  {
+    "revision": "e9b78b3a42733e9071bb",
+    "url": "/static/js/runtime-main.17c7128f.js"
+  },
+  {
+    "revision": "d265065761c93945f70b6995fbf622ea",
+    "url": "/static/media/avatar.d2650657.png"
+  },
+  {
+    "revision": "daeaf731dec30f7b361b6cffbbbc307f",
+    "url": "/static/media/avatar2.daeaf731.png"
+  }
+]);
