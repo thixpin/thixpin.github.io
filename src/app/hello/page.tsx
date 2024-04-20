8e4ef8f4f0059ae710af963f4f8c0a567eecefd8
Soe Thura <thixpin@gmail.com>
2024-04-20 16:10:42 +0700

    added: hello robot

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,12 +1,15 @@
+import { ShowLottie } from '@/components';
+
 const Hello = () => {
-    return (
-      <div className="w-full max-w-xl text-center mt-20 mx-auto">
-          <p className="mt-5 text-3xl capitalize md:text-4xl text-dark-2">
-            Hello, World!
-          </p>
-      </div>
-    );
+  return (
+    <div className="w-full max-w-xl text-center mt-20 mx-auto">
+      <p className="mt-5 text-3xl capitalize md:text-4xl text-dark-2">
+        Hello, World!
+      </p>
+      <ShowLottie path="/lotties/hello.json" className="mx-auto" />
+    </div>
+  );
   };
   
-  export default Hello;
+export default Hello;
   
